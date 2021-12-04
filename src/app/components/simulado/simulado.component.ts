af0bf49438ca40a946fd3afaa07fe0b15ccdf8fd
Caio Nascimento <caionsantos2000@gmail.com>
2021-12-04 17:57:45 -0300

    feat: add id to last created person

--- src/app/components/simulado/simulado.component.ts
+++ src/app/components/simulado/simulado.component.ts
@@ -4,6 +4,7 @@ import { Pergunta } from 'src/app/model/pergunta';
 import { Pessoa } from 'src/app/model/pessoa';
 import { HistoricoService } from 'src/app/service/historico.service';
 import { PerguntaService } from 'src/app/service/pergunta.service';
+import { PersonService } from 'src/app/service/person.service';
 
 @Component({
   selector: 'app-simulado',
@@ -16,11 +17,13 @@ export class SimuladoComponent implements OnInit {
   pergunta: Pergunta = new Pergunta;
   resposta: String = ""
   historico: Historico = new Historico;
+  pessoa: Pessoa = new Pessoa
 
 
   constructor(
     private servicePergunta: PerguntaService,
-    private historicoService: HistoricoService
+    private historicoService: HistoricoService,
+    private pessoaService: PersonService
   ) { }
 
   ngOnInit(): void {
@@ -31,18 +34,23 @@ export class SimuladoComponent implements OnInit {
     this.perguntas = response
     let randomNumber = Math.floor(Math.random() * response.length)
     this.pergunta = this.perguntas[randomNumber];
+
     //remove a pergunta selecionada do array e adiciona a um novo array de perguntas
     this.perguntas.splice(this.perguntas.indexOf(this.perguntas[randomNumber]), 1)
   }
 
   buscarPerguntas() {
+    // Adiciona a ultima pessoa criada para o atributo pessoa
+    this.pessoaService.findAll().subscribe((response) => {
+      this.pessoa = response[response.length - 1]
+    })
     this.servicePergunta.findAll().subscribe(
       (response: Pergunta[]) => {
         //sorteia a pergunta ao renderizar o componente
         this.sorteiaPergunta(response)
       },
       () => {
-      //  alert("Erro!");
+        //  alert("Erro!");
       }
     );
   }
@@ -51,13 +59,21 @@ export class SimuladoComponent implements OnInit {
   enviar() {
     this.historico = new Historico();
     this.historico.pessoa = new Pessoa()
-    this.historico.pessoa.id = 214;
+    this.historico.pessoa.id = this.pessoa.id;
     this.historico.pergunta = this.pergunta;
     this.historico.resposta = this.resposta;
 
     this.historicoService.create(this.historico).subscribe()
 
-    this.sorteiaPergunta(this.perguntas)
+    if (this.perguntas.length !== 0) {
+      console.log("enviou a pergunta")
+      this.sorteiaPergunta(this.perguntas)
+    } else {
+      console.log("caiu aki")
+    }
+
+
+
   }
 
 }
