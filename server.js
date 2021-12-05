7dbbc6ac774005e44cac5a876f15106b0fb28b89
Caio Nascimento <caionsantos2000@gmail.com>
2021-12-05 16:53:57 -0300

    heroku teste9

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static('./dist/project'));
 
-app.get('/*',(req,res)=>res.sendFile('index.html',{root:'dist/project'}))
+app.get('/*', (req, res) => res.sendFile('index.html', { root: 'dist/project' }))
 
-app.listen(8080,console.log("servidor rodando"));
+app.listen(process.env.PORT || 5000);
